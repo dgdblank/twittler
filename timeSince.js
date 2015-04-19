8f823bacb70fd0fb65f12caf0a5f7df307037fb0
Danielle Blank <dgdblank@gmail.com>
2015-04-18 22:42:43 -0700

    Working feed and timestamp. Added bootstrap for styling.

---
+++ timeSince.js
@@ -0,0 +1,35 @@
+//timeSince open source function
+//Slightly modified to account for singular minutes/seconds and "Just now" return val
+function timeSince(date) {
+
+    var seconds = Math.floor((new Date() - date) / 1000);
+
+    var interval = Math.floor(seconds / 31536000);
+
+    if (interval > 1) {
+        return interval + " years";
+    }
+    interval = Math.floor(seconds / 2592000);
+    if (interval > 1) {
+        return interval + " months";
+    }
+    interval = Math.floor(seconds / 86400);
+    if (interval > 1) {
+        return interval + " days";
+    }
+    interval = Math.floor(seconds / 3600);
+    if (interval > 1) {
+        return interval + " hours";
+    }
+    interval = Math.floor(seconds / 60);
+    if (interval === 1) {
+        return "about a minute";
+    }
+    if (interval > 1) {
+        return interval + " minutes";
+    }
+    if (seconds <= 5) {
+        return "less than a minute";
+    }    
+    return  seconds + " seconds";
+}
